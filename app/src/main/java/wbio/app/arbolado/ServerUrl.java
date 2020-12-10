05ba913a6847787a18e196ff2a7ed61f1bb7e87b
Aaron <irving.aacg@gmail.com>
2020-12-10 14:01:47 -0600

    actualizacion del server

--- app/src/main/java/wbio/app/arbolado/ServerUrl.java
+++ app/src/main/java/wbio/app/arbolado/ServerUrl.java
@@ -9,7 +9,7 @@ public class ServerUrl {
     String url;
 
     ServerUrl(){
-        url ="https://arbolado.herokuapp.com";
+        url ="http://arbolmid.monitoreoambiental.org";
     } //direccion del servidor NODE.js
     public String getUrl(){
         return this.url;
@@ -17,7 +17,7 @@ public class ServerUrl {
     public static String getUrlImage(){return "https://www.plataformaciudadanacovid.com/Imagenes/";} //dirección imagenes "Aprende a Identificar"
     public static String getUrlGaleria(){return "https://www.plataformaciudadanacovid.com/galeria_arbolmid/";}//dirección imagenes "Catálogo árboles"
     public static String getUploadImageServer(){return "https://www.plataformaciudadanacovid.com/Arbolado/upload.php";}
-    public static String getUrlServer(){return "https://arbolado.herokuapp.com";}
+    public static String getUrlServer(){return "http://arbolmid.monitoreoambiental.org";}
     //direccion de la base de datos mongo: mongodb+srv://aaron:root@cluster0.ub4h1.mongodb.net/test
 
 }
